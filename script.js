e0ac5c822dddfdb6e8e2ed3f27960918688f0853
adriennecordell <adrienne.cordellac@gmail.com>
2023-02-07 13:10:59 -0600

    got api call to work

--- script.js
+++ script.js
@@ -1,11 +1,69 @@
 // Global variables
-var APIKey = 'dd3c8bf55d91ab4371e0acb375048f53'
+var APIKey = '984f54fcdc9daa25320792f531c6ecc3'
+var input = document.getElementById('search-city')
+var searchButton = document.getElementById('search-button')
+
+function searchCity(){
+    var inputValue = input.val()
+    console.log(inputValue)
+}
 
 
-fetch('https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={dd3c8bf55d91ab4371e0acb375048f53}')
-    .then(res => {
-        return res.json();
-    })
-    .then(data => {
-        console.log(data);
-    });
+
+
+
+// document.getElementById('search-city').value
+
+
+// function searchCity(){
+
+// }
+
+
+
+// function currentWeather(input){
+//     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="+ input +"&units=imperial&appid="+ APIKey;
+//     fetch(queryURL)
+//         .then(response => response.json())
+//         .then(data => {
+//             console.log(data)
+            
+//         })
+// }
+
+
+
+
+
+
+
+
+
+
+
+// data[0].name
+
+// fetch('http://api.openweathermap.org/geo/1.0/direct?q='+ city +'&limit=5&appid='+ APIKey)
+//     .then(res => {
+//         return res.json()
+//     })
+//     .then(data => {
+//         console.log(data[0])
+
+//         var lat = data[0].lat 
+//         var lon = data[0].lon
+//         fetch('http://api.openweathermap.org/data/2.5/forecast?lat='+ lat +'&lon='+ lon +'&appid='+ APIKey)
+//             .then(res => {
+//                 return res.json();
+//             })
+//             .then(data => {
+//                 console.log(data);
+//             });
+//     })
+
+
+
+
+
+
+
